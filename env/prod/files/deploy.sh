9ede3f88eaf08e22a638ab65f6092fe49ccd9ff9
Alaa <alaa@alaas-air.home>
2025-11-05 22:36:50 +0100

    commit deploy script

---
+++ env/prod/files/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_REGION=$(aws ec2 describe-availability-zones --query "AvailabilityZones[0].RegionName" --output text)
+export ACCOUNT_ID AWS_REGION
+
+aws ecr get-login-password --region "$AWS_REGION" \
+| docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+
+
+if [ $# -eq 1 ]; then
+  docker compose pull "$1"
+  docker compose up -d "$1"
+else
+  docker compose pull
+  docker compose up -d
+fi
+
+docker image prune -f
