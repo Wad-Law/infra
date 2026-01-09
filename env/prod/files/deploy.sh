7e214202226b91c9774e40bf1485ce7845a8d0a2
Alaa <zbair.alaa@gmail.com>
2026-01-09 21:22:50 +0100

    fix deploy

--- env/prod/files/deploy.sh
+++ env/prod/files/deploy.sh
@@ -4,6 +4,9 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION=$(aws ec2 describe-availability-zones --query "AvailabilityZones[0].RegionName" --output text)
 export ACCOUNT_ID AWS_REGION
 
+# Ensure we run in the directory containing `docker-compose.yml`
+cd "$(dirname "$0")"
+
 aws ecr get-login-password --region "$AWS_REGION" \
 | docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 
